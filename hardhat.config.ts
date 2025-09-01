1afd4d943eeb9db523c9e95ffb52844edd396132
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-09-01 17:17:19 -0300

    update Hardhat config for improved structure and add setup script in package.json

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,32 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const BESU_PRIVATE_KEY = vars.get("BESU_PRIVATE_KEY")
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+   version: "0.8.28",
+   settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  },
+  networks: {
+    besu: {
+      url: "http://127.0.0.1:8545",
+      gas: "auto",
+      gasPrice: "auto",
+      accounts: [
+        `${BESU_PRIVATE_KEY}`,
+      ],
+    },
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
+  },
 };
 
 export default config;
